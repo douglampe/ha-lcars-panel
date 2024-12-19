a76b7a29dbec2f32904f58820aadfc2de43048aa
Doug Lampe <lampedoug@yahoo.com>
2024-12-18 20:32:10 -0500

    feat: make menu item width flexible and fix font sizes

--- vue/src/MenuItem.ts
+++ vue/src/MenuItem.ts
@@ -3,6 +3,7 @@ export interface MenuItem {
   icon?: string | null
   title: string
   color: string
+  width?: number
   href?: string | null
   action?: string | null
   subAction?: string | null
--- vue/src/components/NumberedMenuItem.vue
+++ vue/src/components/NumberedMenuItem.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import type { MenuItem } from '../MenuItem'
-const props: MenuItem = defineProps<MenuItem>()
+const props: MenuItem = withDefaults(defineProps<MenuItem>(), { width: 5 })
 
-const { index, icon, title, color } = props
+const { index, icon, title, color, width } = props
 </script>
 
 <template>
@@ -11,12 +11,27 @@ const { index, icon, title, color } = props
       <div class="lcars-el l-cap-5" :style="{ backgroundColor: color }"></div>
       <div class="lcars-text-element centered lcars-w-unit">
         <span v-if="icon" v-html="icon" :style="{ color: color }"></span
-        ><span v-else :style="{ color: color }">{{ index }}</span>
+        ><span
+          v-else
+          :style="{
+            color: color,
+            paddingTop: 0,
+            fontSize: 'calc(var(--lcars-unit) * 1)',
+            lineHeight: 'calc(var(--lcars-unit) * 0.9)',
+          }"
+          >{{ index }}</span
+        >
       </div>
     </div>
     <div
-      class="lcars-w-2-minus lcars-h-unit lcars-font-7"
-      :style="{ backgroundColor: color, color: 'var(--lcars-color-black)' }"
+      class="lcars-h-unit"
+      :style="{
+        width: 'calc(var(--lcars-unit) * ' + width + ')',
+        fontSize: 'calc(var(--lcars-unit) * 0.6)',
+        lineHeight: 'calc(var(--lcars-unit) * 0.85)',
+        backgroundColor: color,
+        color: 'var(--lcars-color-black)',
+      }"
     >
       {{ title }}
     </div>
