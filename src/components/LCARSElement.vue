98cd981fa572204e45ae18e97bc3a8e159457385
Doug Lampe <lampedoug@yahoo.com>
2026-03-09 21:44:27 -0400

    feat: use svg for backgrounds

--- src/components/LCARSElement.vue
+++ src/components/LCARSElement.vue
@@ -2,17 +2,10 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 import { navigate } from '@/LocalNav'
-import {
-  alignStyle,
-  borderStyle,
-  colorVar,
-  marginStyle,
-  padStyle,
-  removeUndefined,
-  unitSize,
-} from '@/Layout'
+import { alignStyle, colorVar, marginStyle, padStyle, removeUndefined, unitSize } from '@/Layout'
 import { callService } from '@/HAState'
 import type { ConfigItem } from '@/ConfigItem'
+import { createRadiusSvg } from '@/props/RadiusProps'
 
 const config = defineProps<ConfigItem>()
 
@@ -31,6 +24,8 @@ function onClick() {
 }
 
 const styleObject = computed(() => {
+  const backgroundImageSvg = createRadiusSvg(config)
+
   return removeUndefined({
     ...alignStyle(config.alignContent),
     width: unitSize(config.width),
@@ -39,24 +34,17 @@ const styleObject = computed(() => {
     lineHeight: unitSize(
       config.lineHeight ?? (config.fontSize ? config.fontSize * 1.2 : undefined),
     ),
-    backgroundColor: colorVar(config.color),
+    backgroundImage: backgroundImageSvg
+      ? `url("data:image/svg+xml;utf8,${encodeURIComponent(backgroundImageSvg)}")`
+      : undefined,
+    backgroundSize: backgroundImageSvg ? '100% 100%' : undefined,
+    backgroundRepeat: backgroundImageSvg ? 'no-repeat' : undefined,
+    backgroundColor: backgroundImageSvg ? undefined : colorVar(config.color),
     fill: config.color ? 'color' : undefined,
     color: colorVar(config.textColor),
     flex: config.fill ? 1 : undefined,
     margin: marginStyle(config),
     padding: padStyle(config),
-    borderTopLeftRadius:
-      borderStyle(config.radXTopLeft, config.radYTopLeft) ??
-      (config.capTop || config.capLeft ? unitSize(config.height ?? 1 / 2) : undefined),
-    borderTopRightRadius:
-      borderStyle(config.radXTopRight, config.radYTopRight) ??
-      (config.capTop || config.capRight ? unitSize(config.height ?? 1 / 2) : undefined),
-    borderBottomLeftRadius:
-      borderStyle(config.radXBottomLeft, config.radYBottomLeft) ??
-      (config.capBottom || config.capLeft ? unitSize(config.height ?? 1 / 2) : undefined),
-    borderBottomRightRadius:
-      borderStyle(config.radXBottomRight, config.radYBottomRight) ??
-      (config.capBottom || config.capRight ? unitSize(config.height ?? 1 / 2) : undefined),
     cursor: config.button || config.nav || config.tapAction ? 'pointer' : undefined,
     position:
       config.radXInnerTopLeft ||
@@ -82,59 +70,5 @@ const styleObject = computed(() => {
     v-on="config.nav || config.tapAction ? { click: onClick } : {}"
   >
     <slot></slot>
-    <div
-      v-if="config.radXInnerTopLeft || config.radYInnerTopLeft"
-      style="
-        position: absolute;
-        background-color: var(--lcars-color-inner-rad-bg);
-        bottom: 0;
-        right: 0;
-      "
-      :style="{
-        borderTopLeftRadius: borderStyle(config.radXInnerTopLeft, config.radXInnerTopLeft),
-        width: unitSize(config.radXInnerTopLeft),
-        height: unitSize(config.radYInnerTopLeft),
-      }"
-    ></div>
-    <div
-      v-if="config.radXInnerTopRight || config.radYInnerTopRight"
-      style="
-        position: absolute;
-        background-color: var(--lcars-color-inner-rad-bg);
-        bottom: 0;
-        left: 0;
-      "
-      :style="{
-        borderTopRightRadius: borderStyle(config.radXInnerTopRight, config.radXInnerTopRight),
-        width: unitSize(config.radXInnerTopRight),
-        height: unitSize(config.radYInnerTopRight),
-      }"
-    ></div>
-    <div
-      v-if="config.radXInnerBottomLeft || config.radYInnerBottomLeft"
-      style="
-        position: absolute;
-        background-color: var(--lcars-color-inner-rad-bg);
-        top: 0;
-        right: 0;
-      "
-      :style="{
-        borderBottomLeftRadius: borderStyle(config.radXInnerBottomLeft, config.radXInnerBottomLeft),
-        width: unitSize(config.radXInnerBottomLeft),
-        height: unitSize(config.radYInnerBottomLeft),
-      }"
-    ></div>
-    <div
-      v-if="config.radXInnerBottomRight || config.radYInnerBottomRight"
-      style="position: absolute; background-color: var(--lcars-color-inner-rad-bg); top: 0; left: 0"
-      :style="{
-        borderBottomRightRadius: borderStyle(
-          config.radXInnerBottomRight,
-          config.radXInnerBottomRight,
-        ),
-        width: unitSize(config.radXInnerBottomRight),
-        height: unitSize(config.radYInnerBottomRight),
-      }"
-    ></div>
   </div>
 </template>
--- src/props/RadiusProps.ts
+++ src/props/RadiusProps.ts
@@ -1,3 +1,6 @@
+import { colorList } from '@/ColorList'
+import type { ConfigItem } from '../ConfigItem'
+
 export interface RadiusProps {
   capLeft?: boolean
   capRight?: boolean
@@ -19,4 +22,136 @@ export interface RadiusProps {
   radYInnerBottomLeft?: number
   radXInnerBottomRight?: number
   radYInnerBottomRight?: number
+}
+
+function scaleValue(value?: number, defaultValue = 0) {
+  return (value ?? defaultValue) * 10
+}
+
+//TODO: Resolve color names to hex values
+function resolveColor(color?: string) {
+  if (color) {
+    if (
+      color.startsWith('#') ||
+      color.startsWith('rgb') ||
+      color.startsWith('hsl') ||
+      color.startsWith('var(')
+    ) {
+      return color
+    }
+
+    const colorKey = color.replace(/\-/g, '_')
+    if (colorList.hasOwnProperty(colorKey)) {
+      return colorList[colorKey]
+    }
+  }
+
+  return '#ffc152'
+}
+
+function createPath(config: ConfigItem, width: number, height: number) {
+  const path = []
+
+  const offsetTL = scaleValue(config.radXTopLeft ?? config.radXInnerBottomRight, 0)
+  const offsetTR = scaleValue(config.radXTopRight ?? config.radXInnerBottomLeft, 0)
+  const offsetRT = scaleValue(
+    config.radYTopRight ??
+      config.radXTopRight ??
+      config.radYInnerBottomLeft ??
+      config.radXInnerBottomLeft,
+    0,
+  )
+  const offsetRB = scaleValue(
+    config.radYBottomRight ??
+      config.radXBottomRight ??
+      config.radYInnerTopLeft ??
+      config.radXInnerTopLeft,
+    0,
+  )
+  const offsetBR = scaleValue(config.radXBottomRight ?? config.radXInnerTopLeft, 0)
+  const offsetBL = scaleValue(config.radXBottomLeft ?? config.radXInnerTopRight, 0)
+  const offsetLB = scaleValue(
+    config.radYBottomLeft ??
+      config.radXBottomLeft ??
+      config.radYInnerTopRight ??
+      config.radXInnerTopRight,
+    0,
+  )
+  const offsetLT = scaleValue(
+    config.radYTopLeft ??
+      config.radXTopLeft ??
+      config.radYInnerBottomRight ??
+      config.radXInnerBottomRight,
+    0,
+  )
+
+  const startX = 0
+  const startY = offsetLT
+
+  path.push(`M ${startX} ${startY}`)
+  if (config.radXTopLeft) {
+    path.push(`q 0 ${-offsetLT} ${offsetTL} ${-offsetLT}`)
+  } else if (config.radXInnerBottomRight) {
+    path.push(`q ${offsetTL} 0 ${offsetTL} ${-offsetLT}`)
+  }
+  path.push(`L ${width - offsetTR} 0`)
+  if (config.radXTopRight) {
+    path.push(`q ${offsetTR} 0 ${offsetTR} ${offsetRT}`)
+  } else if (config.radXInnerBottomLeft) {
+    path.push(`q 0 ${offsetRT} ${offsetTR} ${offsetRT}`)
+  }
+  path.push(`L ${width} ${height - offsetRB}`)
+  if (config.radXBottomRight) {
+    path.push(`q 0 ${offsetRB} ${-offsetBR} ${offsetRB}`)
+  } else if (config.radXInnerTopLeft) {
+    path.push(`q ${-offsetBR} 0 ${-offsetBR} ${offsetRB}`)
+  }
+  path.push(`L ${offsetBL} ${height}`)
+  if (config.radXBottomLeft) {
+    path.push(`q ${-offsetBL} 0 ${-offsetBL} ${-offsetLB}`)
+  } else if (config.radXInnerTopRight) {
+    path.push(`q 0 ${-offsetLB} ${-offsetBL} ${-offsetLB}`)
+  }
+
+  path.push('Z')
+  return path.join(' ')
+}
+
+export function createRadiusSvg(config: ConfigItem): string | undefined {
+  if (!Object.keys(config).some((key) => key.startsWith('radX') || key.startsWith('radY'))) {
+    return
+  }
+
+  if (typeof config.width !== 'number' || typeof config.height !== 'number') {
+    return
+  }
+
+  const width = scaleValue(config.width)
+  const height = scaleValue(config.height)
+  const fill = resolveColor(config.color)
+  console.log(config.color, fill)
+
+  const processedConfig = {
+    ...config,
+  }
+
+  if (config.capLeft) {
+    processedConfig.radXTopLeft = config.height / 2
+    processedConfig.radXBottomLeft = processedConfig.radXTopLeft
+  }
+  if (config.capRight) {
+    processedConfig.radXTopRight = config.height / 2
+    processedConfig.radXBottomRight = processedConfig.radXTopRight
+  }
+  if (config.capTop) {
+    processedConfig.radYTopLeft = config.width / 2
+    processedConfig.radYTopRight = processedConfig.radYTopLeft
+  }
+  if (config.capBottom) {
+    processedConfig.radYBottomLeft = config.width / 2
+    processedConfig.radYBottomRight = processedConfig.radYBottomLeft
+  }
+
+  const pathData = createPath(processedConfig, width, height)
+  return `<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}" viewBox="0 0 ${width} ${height}" preserveAspectRatio="none"><path d="${pathData}" fill="${fill}"/></svg>`
 }
