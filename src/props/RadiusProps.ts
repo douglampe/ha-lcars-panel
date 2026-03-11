3d34f80695fb2268d395804a6be9ddc805b75fef
Doug Lampe <douglampe@users.noreply.github.com>
2026-03-11 08:59:33 -0400

    82 svg backgrounds (#89)
    * feat: use svg for backgrounds
    
    * fix: handle auto-sized elements with curves

--- src/props/RadiusProps.ts
+++ src/props/RadiusProps.ts
@@ -1,3 +1,6 @@
+import { colorList } from '@/ColorList'
+import type { ConfigItem } from '../ConfigItem'
+
 export interface RadiusProps {
   capLeft?: boolean
   capRight?: boolean
@@ -19,4 +22,164 @@ export interface RadiusProps {
   radYInnerBottomLeft?: number
   radXInnerBottomRight?: number
   radYInnerBottomRight?: number
+}
+
+function scaleValue(value?: number, defaultValue = 0) {
+  return (value ?? defaultValue) * 10
+}
+
+function unscale(value?: number, defaultValue = 0) {
+  return (value ?? defaultValue) / 10
+}
+
+//TODO: Resolve color names to hex values
+function resolveColor(color?: string) {
+  if (color) {
+    if (
+      color.startsWith('#') ||
+      color.startsWith('rgb') ||
+      color.startsWith('hsl') ||
+      color.startsWith('var(')
+    ) {
+      return color
+    }
+
+    const colorKey = color.replace(/\-/g, '_')
+    if (colorList.hasOwnProperty(colorKey)) {
+      return colorList[colorKey]
+    }
+  }
+
+  return '#ffc152'
+}
+
+function createPath(config: ConfigItem, width: number, height: number) {
+  const path = []
+
+  const offsetTL = scaleValue(config.radXTopLeft ?? config.radXInnerBottomRight, 0)
+  const offsetTR = scaleValue(config.radXTopRight ?? config.radXInnerBottomLeft, 0)
+  const offsetRT = scaleValue(
+    config.radYTopRight ??
+      config.radXTopRight ??
+      config.radYInnerBottomLeft ??
+      config.radXInnerBottomLeft,
+    0,
+  )
+  const offsetRB = scaleValue(
+    config.radYBottomRight ??
+      config.radXBottomRight ??
+      config.radYInnerTopLeft ??
+      config.radXInnerTopLeft,
+    0,
+  )
+  const offsetBR = scaleValue(config.radXBottomRight ?? config.radXInnerTopLeft, 0)
+  const offsetBL = scaleValue(config.radXBottomLeft ?? config.radXInnerTopRight, 0)
+  const offsetLB = scaleValue(
+    config.radYBottomLeft ??
+      config.radXBottomLeft ??
+      config.radYInnerTopRight ??
+      config.radXInnerTopRight,
+    0,
+  )
+  const offsetLT = scaleValue(
+    config.radYTopLeft ??
+      config.radXTopLeft ??
+      config.radYInnerBottomRight ??
+      config.radXInnerBottomRight,
+    0,
+  )
+
+  const startX = 0
+  const startY = offsetLT
+
+  path.push(`M ${startX} ${startY}`)
+  if (config.radXTopLeft) {
+    path.push(`q 0 ${-offsetLT} ${offsetTL} ${-offsetLT}`)
+  } else if (config.radXInnerBottomRight) {
+    path.push(`q ${offsetTL} 0 ${offsetTL} ${-offsetLT}`)
+  }
+  path.push(`L ${width - offsetTR} 0`)
+  if (config.radXTopRight) {
+    path.push(`q ${offsetTR} 0 ${offsetTR} ${offsetRT}`)
+  } else if (config.radXInnerBottomLeft) {
+    path.push(`q 0 ${offsetRT} ${offsetTR} ${offsetRT}`)
+  }
+  path.push(`L ${width} ${height - offsetRB}`)
+  if (config.radXBottomRight) {
+    path.push(`q 0 ${offsetRB} ${-offsetBR} ${offsetRB}`)
+  } else if (config.radXInnerTopLeft) {
+    path.push(`q ${-offsetBR} 0 ${-offsetBR} ${offsetRB}`)
+  }
+  path.push(`L ${offsetBL} ${height}`)
+  if (config.radXBottomLeft) {
+    path.push(`q ${-offsetBL} 0 ${-offsetBL} ${-offsetLB}`)
+  } else if (config.radXInnerTopRight) {
+    path.push(`q 0 ${-offsetLB} ${-offsetBL} ${-offsetLB}`)
+  }
+  path.push('Z')
+
+  return path.join(' ')
+}
+
+export function createRadiusSvg(
+  config: ConfigItem,
+  actualWidth: number | undefined,
+  actualHeight: number | undefined,
+): string | undefined {
+  if (!Object.keys(config).some((key) => key.startsWith('radX') || key.startsWith('radY'))) {
+    return
+  }
+
+  let width = typeof config.width === 'number' ? scaleValue(config.width) : undefined
+  let height = typeof config.height === 'number' ? scaleValue(config.height) : undefined
+
+  if (actualWidth && actualHeight) {
+    if (width && height) {
+      if (
+        parseFloat((actualWidth / width).toFixed(2)) >
+        parseFloat((actualHeight / height).toFixed(2))
+      ) {
+        width = (actualWidth * height) / actualHeight
+      } else if (
+        parseFloat((actualWidth / width).toFixed(2)) <
+        parseFloat((actualHeight / height).toFixed(2))
+      ) {
+        height = (actualHeight * width) / actualWidth
+      }
+    } else if (width) {
+      height = (actualHeight * width) / actualWidth
+    } else if (height) {
+      width = (actualWidth * height) / actualHeight
+    }
+  }
+
+  if (typeof width !== 'number' || typeof height !== 'number') {
+    return
+  }
+
+  const fill = resolveColor(config.color)
+
+  const processedConfig = {
+    ...config,
+  }
+
+  if (config.capLeft) {
+    processedConfig.radXTopLeft = unscale(height / 2)
+    processedConfig.radXBottomLeft = processedConfig.radXTopLeft
+  }
+  if (config.capRight) {
+    processedConfig.radXTopRight = unscale(height / 2)
+    processedConfig.radXBottomRight = processedConfig.radXTopRight
+  }
+  if (config.capTop) {
+    processedConfig.radYTopLeft = unscale(width / 2)
+    processedConfig.radYTopRight = processedConfig.radYTopLeft
+  }
+  if (config.capBottom) {
+    processedConfig.radYBottomLeft = unscale(width / 2)
+    processedConfig.radYBottomRight = processedConfig.radYBottomLeft
+  }
+
+  const pathData = createPath(processedConfig, width, height)
+  return `<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}" viewBox="0 0 ${width} ${height}" preserveAspectRatio="none"><path d="${pathData}" fill="${fill}"/></svg>`
 }
