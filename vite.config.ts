dfa8cc839a49d4888c019d76f347bc2d441d1eaa
Doug Lampe <lampedoug@yahoo.com>
2026-03-20 07:19:28 -0400

    wip: wip on template support

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+    'process.env': {},
+  },
   base: '/ha-lcars-panel',
   build: {
     minify: false,
